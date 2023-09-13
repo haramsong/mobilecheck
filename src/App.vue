a86115f6bf410a1b470ba64a37d8a6f0c24ccada
haramsong <alakazam1324@gmail.com>
2023-09-13 14:46:36 +0900

    merge

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    접속 기기 종류: {{ deviceInfo ? '모바일' : 'PC' }} <br>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+console.log(navigator.userAgent)
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      deviceInfo: '',
+    }
+  },
+  created() {
+    if (this.deviceInfo == true) {
+      window.location.href = "https://google.com"
+    }
+  },
+  methods: {
+     isMobile() {
+      const info = navigator.userAgent;
+      var flag = false;
+
+      if (info.indexOf("iPhone") > -1
+        || info.indexOf("Android") > -1
+        || info.indexOf("iPad") > -1
+        || info.indexOf("iPod") > -1
+      ) {
+        flag = true;
+      }
+      this.deviceInfo = flag;
+    },
+  },
+
 }
 </script>
 
